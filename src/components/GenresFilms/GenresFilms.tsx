9134519c465d95cbcc7759cfa9c66c8764dab924
ruz50 <sruzan50@gmail.com>
2024-06-11 10:50:29 +0400

    Your commit message

--- src/components/GenresFilms/GenresFilms.tsx
+++ src/components/GenresFilms/GenresFilms.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import  { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../../store/hooks'
 import { fetchGenresFilm } from '../../store/slices/GenresfilmSlice'
 import { useParams } from 'react-router-dom'
